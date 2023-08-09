6689f349feab9fa5330e2b73bcc0f38119c42d2d
RoanMacmillan <hibee94@gmail.com>
2023-08-09 13:32:13 +0100

    fixes

--- src/containers/AboutHero.tsx
+++ src/containers/AboutHero.tsx
@@ -1,23 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const AboutHero = () => {
   return (
-      <div className="relative  mx-auto mt-[50px] customMd:mt-[133px] lg:mt-[184px] lg:max-w-[1440px] px-4 customMd:px-20">
-      <h1 className="text-[60px] font-light leading-[1] tracking-[-2px] lg:text-[80px]">Frontend<br></br>
+    <div className="relative  mx-auto mt-[50px] px-4 customMd:mt-[133px] customMd:px-20 lg:mt-[184px] lg:max-w-[1440px]">
+      <h1 className="text-[60px] font-light leading-[1] tracking-[-2px] lg:text-[80px]">
+        Frontend<br></br>
         <span className="font-bold">Developer</span>
-      <div className="mt-6 h-[5px] w-14 bg-customBlack"></div>
-
+        <div className="mt-6 h-[5px] w-14 bg-customBlack"></div>
       </h1>
 
-        <div className='mt-[50px]'>
-      <h2 className='font-bold text-3xl'>About me</h2>
-      <p className='text-customGray mt-4 max-w-[550px]'>Hi, I'm Roan Macmillan. I'm a frontend developer looking for a new role. I'm based in Edinburgh and open to working remotely.</p>
-      <p className="mt-[30px] font-semibold">Read More</p>
-      <div className="mt-[5px] h-[2px] w-24 bg-gray-200"></div>
+      <div className="mt-[50px]">
+        <h2 className="text-3xl font-bold">About me</h2>
+        <p className="mt-4 max-w-[550px] text-customGray">
+          Hi, I'm Roan Macmillan. I'm a frontend developer looking for a new
+          role. I'm based in Edinburgh and open to working remotely.
+        </p>
+        <p className="mt-[30px] font-semibold">Read More</p>
+        <div className="mt-[5px] h-[2px] w-24 bg-gray-200"></div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default AboutHero
+export default AboutHero;
--- src/containers/FormContainer.tsx
+++ src/containers/FormContainer.tsx
@@ -2,33 +2,24 @@ import React from "react";
 import Form from "@/components/Form";
 import Image from "next/image";
 import mail from "../../public/assets/icons/icon-mail.svg";
-import photo from '../../public/assets/images/photos/footer-hero.webp'
+import photo from "../../public/assets/images/photos/footer-hero.webp";
 
 const FormContainer = () => {
   return (
     <div className="mx-auto mt-[50px] max-w-[1280px] customMd:mt-[100px]">
-
- 
-
-
-
-      <h1 className="text-[60px] mt-12 lg:mt-20 font-light leading-[1] tracking-[-2px] lg:text-[80px]">
+      <h1 className="mt-12 text-[60px] font-light leading-[1] tracking-[-2px] lg:mt-20 lg:text-[80px]">
         Let's<br className="lg:hidden"></br>
         <span className="font-bold lg:ml-[10px]">Talk</span>
       </h1>
       <div className="mt-6 h-[5px] w-14 bg-customBlack"></div>
 
-      <div className="mt-12 lg:mt-16 customMd:flex">
+      <div className="mt-12 customMd:flex lg:mt-16">
         <div className="customMd:w-[40%]">
           <h2 className="text-2xl font-bold">Contact form</h2>
           <p className="mt-4 text-customGray">
             Got a project in mind? Find my contact info below or feel free to
             fill out the form below with your questions.
           </p>
-
-
-
-          
         </div>
 
         <Form />
@@ -43,7 +34,6 @@ const FormContainer = () => {
         </p>
       </div>
       <div className="mt-[60px] block h-[1px] w-full bg-gray-200 sm:hidden"></div>
-
     </div>
   );
 };
