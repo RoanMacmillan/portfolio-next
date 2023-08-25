5383387d40f6de4187b23996c06ab75cabd966e1
RoanMacmillan <hibee94@gmail.com>
2023-08-25 19:27:56 +0100

    ...

--- functions/sendEmail.js
+++ functions/sendEmail.js
@@ -2,7 +2,58 @@
 
 const nodemailer = require("nodemailer");
 
+
+const rateLimitWindow = 60 * 10000; // 60 seconds in milliseconds
+const maxRequestsPerWindow = 5; // Max number of requests per IP in the rate limit window
+
+const requestCounts = {}; // Store request counts per IP address
+
+
+function isRateLimited(ip) {
+  const currentTime = Date.now();
+  const requests = requestCounts[ip] || []; 
+
+  // Remove requests older than the rate limit window
+  while (requests.length > 0 && requests[0] < currentTime - rateLimitWindow) {
+    requests.shift();
+  }
+
+  console.log(`IP: ${ip}, requests count: ${requests.length}`); // Log the number of requests from the IP address
+
+  // If the number of remaining requests is less than the max allowed, record the new request and return false (not rate-limited)
+  if (requests.length < maxRequestsPerWindow) {
+    requests.push(currentTime);
+    requestCounts[ip] = requests;
+    console.log(`Not rate-limited: ${ip}`); // Log that the IP is not rate-limited
+    return false;
+  }
+
+  // If the number of remaining requests is equal to or greater than the max allowed, return true (rate-limited)
+  console.log(`Rate-limited: ${ip}`); // Log that the IP is rate-limited
+  return true;
+}
+
 exports.handler = async (event) => {
+
+
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode: 405,
+      body: "Method Not Allowed",
+    };
+  }
+
+  const clientIp = event.headers["client-ip"];
+
+  if (isRateLimited(clientIp)) {
+    return {
+      statusCode: 429,
+      body: "Too Many Requests",
+    };
+  }
+
+
+
   const formData = JSON.parse(event.body);
 
   const YOUR_EMAIL = process.env.YOUR_EMAIL;
@@ -27,6 +78,8 @@ exports.handler = async (event) => {
   };
 
   try {
+    console.log("Sending email:", mailOptions); // Add this line
+
     await transporter.sendMail(mailOptions);
     return {
       statusCode: 200,
